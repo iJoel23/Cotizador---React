54c4196ba7183becb757d4d070895c9f5d9c61aa
Joel Leon <120341930+iJoel23@users.noreply.github.com>
2024-01-07 17:45:56 -0500

    init

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,12 @@
+import '../styles/Spinner.css'
+
+function Spinner() {
+  return (
+<div className="spinner">
+  <div className="cube1"></div>
+  <div className="cube2"></div>
+</div>
+  )
+}
+
+export default Spinner
